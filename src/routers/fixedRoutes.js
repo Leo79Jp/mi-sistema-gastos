4a3c651cebee43ce263f105e4356e6c9714cbcfb
Leo79Jp <leojparodi@gmail.com>
2026-07-31 03:01:36 -0300

    volvi para atras con el boton de fijos

--- src/routers/fixedRoutes.js
+++ src/routers/fixedRoutes.js
@@ -5,10 +5,6 @@ const { isAuthenticated } = require('../middlewares/authMiddleware');
 
 router.get('/', isAuthenticated, fixedController.getFixedExpenses);
 router.post('/add', isAuthenticated, fixedController.addFixedExpense);
-
-// 🔄 NUEVA RUTA PARA GENERAR LOS GASTOS DEL MES (Va antes de /delete/:id)
-router.post('/generate', isAuthenticated, fixedController.generateMonthlyExpenses);
-
 router.post('/delete/:id', isAuthenticated, fixedController.deleteFixedExpense);
 
 module.exports = router;
